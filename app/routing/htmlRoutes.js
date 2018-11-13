67fd17ce00d61f1d14b6767a1b10a5893ef395e9
SeanB <Spb4578@gmail.com>
2018-11-13 16:36:43 -0600

    new setup

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+
+let path = require('path');
+
+
+
+module.exports = function (app) {
+	
+	app.get('/', function (request, result) {
+		result.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+	app.get('/survey', function (request, result) {
+		result.sendFile(path.join(__dirname + '/../public/servey.html'));
+	});	
+};
